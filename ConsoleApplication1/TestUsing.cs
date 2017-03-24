79069fe55b35c08089e1bd340675e8af9ee8f06a
杨旋 <yx0588@175game.com>
2017-03-24 17:12:47 +0800

    s

--- ConsoleApplication1/TestUsing.cs
+++ ConsoleApplication1/TestUsing.cs
@@ -5,33 +5,62 @@ using System.Text;
 
 using System.Text.RegularExpressions;
 
-class TestUsing
-{
-    class MyTest : IDisposable
-    {
-        public MyTest(string name)
-        {
+class TestUsing {
+    class MyBase : IDisposable {
+        public MyBase(string name) {
             mName = name;
         }
         string mName = "";
-        public void Dispose()
-        {
-            Console.WriteLine("--- MyTest Dispose, name:{0}", mName);
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing) {
+            if (disposing) {
+                // Free other state (managed objects).
+                Console.WriteLine("--- MyBase Dispose, name:{0}", mName);
+            }
+            // Free your own state (unmanaged objects).
+            // Set large fields to null.
+        }
+
+        ~MyBase() {
+            // Simply call Dispose(false).
+            Dispose(false);
         }
     };
 
+    // Design pattern for a derived class.
+    class MyDerived : MyBase {
+        public MyDerived(string name) : base(name) {
+        }
+
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
+                // Release managed resources.
+            }
+            // Release unmanaged resources.
+            // Set large fields to null.
+            // Call Dispose on your base class.
+            base.Dispose(disposing);
+        }
+        // The derived class does not have a Finalize method
+        // or a Dispose method without parameters because it inherits
+        // them from the base class.
+    }
+
     /// <summary>
     /// https://msdn.microsoft.com/zh-cn/library/yh598w02.aspx
+    /// https://msdn.microsoft.com/zh-cn/library/b1yfkh5e(v=vs.85).aspx
     /// 实现类似c++中超过作用域后析构的效果，但c#中超过 using 只是掉了 Dispose 接口，对象还是没有被gc
     /// 实际应用如 FileStream 等，不需要去主动调用 Close()
     /// </summary>
-    public static void test1()
-    {
+    public static void test1() {
         Console.WriteLine("---test1 begin");
 
-        using (MyTest mt1 = new MyTest("hello"), 
-                      mt2 = new MyTest("world"))
-        {
+        using (MyDerived mt1 = new MyDerived("hello"),
+                      mt2 = new MyDerived("world")) {
             Console.WriteLine("--- instantiate mt1 mt2");
         }
 
